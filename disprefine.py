a47c1d4d4f00a735b15e21f6923e351138f72841
william <williamwei9707@gmail.com>
2024-09-24 10:41:06 +0800

    add experiment notebooks.

--- disprefine.py
+++ disprefine.py
@@ -34,19 +34,22 @@ if __name__ == "__main__":
     m_types = ['vits', 'vitb', 'vitl']
     # model_path="D:/william/checkpoints/depth-anything/v2/depth_anything_v2_vitb.pth"
     # subfolders = [11, 12, 19]
-    top_dir = "D:/william/data/disp-refine/ori"
+    top_dir = "D:/william/data/disp-refine/_feynman_camera_M1F0001222_leftir_image_rect_color"
     subfolders = [f for f in os.listdir(top_dir) if os.path.isdir(os.path.join(top_dir, f))]
+    subfiles = [f for f in os.listdir(top_dir) if os.path.isfile(os.path.join(top_dir, f)) and f.endswith(".png")]
     for type in m_types:
         print(f"processing {type} right now ...")
         model_path = f"D:/william/checkpoints/depth-anything/v2/depth_anything_v2_{type}.pth"
-        for sub in subfolders:
+        for sub in subfiles:
+            print(f"processing {sub} ...")
             # path = f"{top_dir}/{sub}/output_0222_agg_mask.mat"
             # disp = load_disp_from_mat(path, "out/s1out/cleanDispL")
             # print(disp.T.shape)
             # disp_save_path = f'{top_dir}/{sub}/output_0222_agg.npy'
             # save_numpy_array(disp.T, disp_save_path)
 
-            image_path = f"{top_dir}/{sub}/left.png"
+            # image_path = f"{top_dir}/{sub}/left.bmp"
+            image_path = f"{top_dir}/{sub}"
             image = cv2.imread(image_path)
             # predicted = inference_single(image, model_path)
             predicted = inference_single_v2(image, model_path, encoder=f'{type}')
@@ -54,6 +57,7 @@ if __name__ == "__main__":
         # print(predicted.shape)
         # save_path = "data/11/output_0222_DL.mat"  
         # save_numpy_array_to_matlab(predicted, save_path)
-            predicted_save_path = f"{top_dir}/{sub}/dam_v2_{type}_out.npy"
+            predicted_save_path = f"{top_dir}/output/dam_v2_{type}_out.npy"
             save_numpy_array(predicted, predicted_save_path)
+            break
     print("=====================> done! <=====================")  
